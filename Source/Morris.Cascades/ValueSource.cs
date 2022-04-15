1cca6cbe3a5170b7848e74c6923cf872dd950214
mrpmorris <mrpmorris@gmail.com>
2022-04-15 19:15:15 +0100

    Add optional EqualityComparer<T> to ValueSource constructor

--- Source/Morris.Cascades/ValueSource.cs
+++ Source/Morris.Cascades/ValueSource.cs
@@ -2,12 +2,23 @@
 
 public class ValueSource<T> : ChangeNotifierBase, ISource<T>
 {
+	private readonly IEqualityComparer<T>? EqualityComparer;
+
+	public ValueSource(IEqualityComparer<T>? equalityComparer = null)
+	{
+		EqualityComparer = equalityComparer;
+	}
+
+
 	private T _value = default!;
 	public T Value
 	{
 		get => _value;
 		set
 		{
+			if (EqualityComparer?.Equals(_value, value) == true)
+				return;
+
 			_value = value;
 			NotifySubscribers();
 		}

4f040cfdb789aed84b1dfc5c8c61f7251d50597a
mrpmorris <mrpmorris@gmail.com>
2022-04-17 11:52:42 +0100

    Refactoring demo

--- Source/Tutorials/BlazorCachedCalculation/Pages/Spreadsheet.razor.cs
+++ Source/Tutorials/BlazorCachedCalculation/Pages/Spreadsheet.razor.cs
@@ -5,28 +5,33 @@ namespace BlazorCachedCalculation.Pages;
 
 public partial class Spreadsheet : IDisposable
 {
-	private bool GenerateAutoValues = false;
-	private Timer GenerateAutoValuesTimer = null;
+	private bool IncrementRandomCells = false;
+	private Timer IncrementRandomCellsTimer = null;
 	private SheetData Sheet = new SheetData(5, 5);
 
-	private string ToggleAutoValuesButtonCaption => GenerateAutoValues ? "Stop generating auto values" : "Start generating auto values";
-	private string ToggleAutoValuesButtonClass => GenerateAutoValues ? "btn-secondary" : "btn-primary";
+	private string IncrementRandomCellsButtonCaption => IncrementRandomCells ? "Stop incrementing random cells" : "Start incrementing random cells";
+	private string IncrementRandomCellsButtonClass => IncrementRandomCells ? "btn-secondary" : "btn-primary";
 
 	private void ToggleAutoValues()
 	{
-		GenerateAutoValues = !GenerateAutoValues;
-		if (GenerateAutoValues)
-			GenerateAutoValuesTimer = new Timer(GenerateAutoValue, null, 0, 250);
+		IncrementRandomCells = !IncrementRandomCells;
+		if (IncrementRandomCells)
+			IncrementRandomCellsTimer = new Timer(GenerateAutoValue, null, 0, 250);
 		else
-			GenerateAutoValuesTimer.Dispose();
+			IncrementRandomCellsTimer.Dispose();
 	}
 
 	void IDisposable.Dispose()
 	{
-		GenerateAutoValuesTimer.Dispose();
+		IncrementRandomCellsTimer.Dispose();
 	}
 
 	private void GenerateAutoValue(object? _)
+	{
+		IncrementRandomCell();
+	}
+
+	private void IncrementRandomCell()
 	{
 		var row = Random.Shared.Next(Sheet.RowCount - 1);
 		var column = Random.Shared.Next(Sheet.ColumnCount - 1);
